ca25e3f26ab16dd6b8b15cccccaaf38be9bb34fc
EazyM93 <centini.manuel.93@gmail.com>
2023-09-11 00:16:42 +0200

    Create Home / Login / Register Component

--- ShelterBooksApp/src/app/app.module.ts
+++ ShelterBooksApp/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { HomepageComponent } from './components/homepage/homepage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomepageComponent
   ],
   imports: [
     BrowserModule
