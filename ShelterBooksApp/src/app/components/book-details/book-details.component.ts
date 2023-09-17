7cb5f782d37ec90e569cf4182eba9b2417ad1a0f
EazyM93 <centini.manuel.93@gmail.com>
2023-09-17 15:29:59 +0200

    Book Details Structure

--- ShelterBooksApp/src/app/components/book-details/book-details.component.ts
+++ ShelterBooksApp/src/app/components/book-details/book-details.component.ts
@@ -10,8 +10,20 @@ import { ApiShelterService } from 'src/app/service/api-shelter.service';
 export class BookDetailsComponent implements OnInit {
 
   idBook: string = '';
+  isbn: string = '';
+	title: string = '';
+  description: string = '';
   coverLink: string = '';
-  title: string = '';
+  author: string = '';
+  publisher: string = '';
+	pages: number = 0;
+  price: number = 0;
+  publicationYear: number = 0;
+  genre: string = '';
+  availableCopies: number = 0;
+	availableEbook: string = 'UNAVAILABLE';
+	ebookSize: number = 0;
+  ebookPrice: number = 0;
 
   constructor(private route: ActivatedRoute, private shelterService: ApiShelterService) { }
 
@@ -20,8 +32,20 @@ export class BookDetailsComponent implements OnInit {
       this.idBook = idRecived['id'];
       this.shelterService.getBook(this.idBook)
         .subscribe(response => {
-          this.coverLink = response.coverLink;
+          this.isbn = response.isbn;
           this.title = response.title;
+          this.description = response.description;
+          this.coverLink = response.coverLink;
+          this.author = response.author;
+          this.publisher = response.publisher;
+          this.pages = response.pages;
+          this.price = response.price;
+          this.publicationYear = response.publicationYear;
+          this.genre = response.genre.toLowerCase();
+          this.availableCopies = response.availableCopies;
+          this.availableEbook = response.availableEbook;
+          this.ebookSize = response.ebookSize;
+          this.ebookPrice = response.ebookPrice;
         });
     });
   }
